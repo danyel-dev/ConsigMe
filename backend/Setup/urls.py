010a261c167d27d064d1116beec46692aa2205c7
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 <dannykill4221x@gmail.com>
2023-03-13 18:17:47 -0300

    starting over from scratch

--- backend/Setup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import CreateUser
-
-
-urlpatterns = [
-    path('register/', CreateUser.as_view()),
-    path('admin/', admin.site.urls),
-]
