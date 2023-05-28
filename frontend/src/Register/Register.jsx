ca4298f0bda60b4fb33b7aa9e1f1453476372500
danyel-dev <dannykill4221x@gmail.com>
2023-05-27 23:29:53 -0300

    adding style in register form

--- frontend/src/Register/Register.jsx
+++ frontend/src/Register/Register.jsx
@@ -21,23 +21,34 @@ export default function Register() {
         <>
             <main className={styles.mainRegister}>
                 <form onSubmit={handleSubmitForm} className={styles.formRegister}>
-                    <label htmlFor="firstname" className={styles.labelForm}>Nome</label>
-                    <input id="firstname" type="text" className={styles.inputForm} />
-                   
-                    <label htmlFor="lastname" className={styles.labelForm}>Sobrenome</label>
-                    <input id='lastname' type="text" className={styles.inputForm} />
+                    <div className={styles.fullnameForm}>
+                        <div>
+                            <label htmlFor="firstname" className={styles.labelForm}>Nome</label>
+                            <input id="firstname" type="text" className={styles.inputForm} placeholder='Primeiro nome' />
+                        </div>
+
+                        <div>
+                            <label htmlFor="lastname" className={styles.labelForm}>Sobrenome</label>
+                            <input id='lastname' type="text" className={styles.inputForm} placeholder='Último nome' />
+                        </div>
+                    </div>
 
                     <label htmlFor="email" className={styles.labelForm}>Email</label>
-                    <input id="email" type="email" className={styles.inputForm} />
+                    <input id="email" type="email" className={styles.inputForm} placeholder='email@email.com' />
                     
                     <label htmlFor="username" className={styles.labelForm}>Nome de usuário</label>
-                    <input id='username' type="text" className={styles.inputForm} />
+                    <input id='username' type="text" className={styles.inputForm} placeholder='Entre com um nome de usuário' />
 
                     <label htmlFor="passwordOne" className={styles.labelForm}>Senha</label>
-                    <input id='passwordOne' type="password" className={styles.inputForm} />
+                    <input id='passwordOne' type="password" className={styles.inputForm} placeholder='Informe uma senha' />
 
                     <label htmlFor="passwordTwo" className={styles.labelForm}>Confirmar senha</label>
-                    <input id='passwordTwo' type="password" className={styles.inputForm} />
+                    <input id='passwordTwo' type="password" className={styles.inputForm} placeholder='Repita novamente a senha' />
+
+                    <div className={styles.submitDiv}>
+                        <button className={styles.buttonForm}>Cadastrar</button>
+                        <small className={styles.small}>Já possui uma conta? <a href="#">Entre nela</a></small>
+                    </div>
                 </form>
             </main>
         </>
