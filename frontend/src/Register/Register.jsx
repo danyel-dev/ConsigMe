8fc79d198e5bd674054020963a8876cb6ac040f4
danyel-dev <dannykill4221x@gmail.com>
2023-05-27 22:08:28 -0300

    creating register form

--- frontend/src/Register/Register.jsx
+++ frontend/src/Register/Register.jsx
@@ -1,25 +1,45 @@
-import axios from "axios";
+// import axios from "axios";
+import styles from './register.module.css'
 
 
 export default function Register() {
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Token ' + localStorage.getItem('token')
-        }
-    }
+    // const config = {
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Authorization': 'Token ' + localStorage.getItem('token')
+    //     }
+    // }
 
-    function handleClick() {
-        axios.post("http://127.0.0.1:8000/users/", 
-        {username: "amelia", email:"amelia@gmail.com", password: "santoscdp"}, config).then(response => {
-            console.log(response)
-        })  
+    function handleSubmitForm() {
+        // axios.post("http://127.0.0.1:8000/users/", 
+        // {username: "amelia", email:"amelia@gmail.com", password: "santoscdp"}, config).then(response => {
+        //     console.log(response)
+        // })  
     }
 
     return (
-        <div>
-            <p>hello</p>
-            <button onClick={handleClick}>click</button>
-        </div>
+        <>
+            <main className={styles.mainRegister}>
+                <form onSubmit={handleSubmitForm} className={styles.formRegister}>
+                    <label htmlFor="firstname" className={styles.labelForm}>Nome</label>
+                    <input id="firstname" type="text" className={styles.inputForm} />
+                   
+                    <label htmlFor="lastname" className={styles.labelForm}>Sobrenome</label>
+                    <input id='lastname' type="text" className={styles.inputForm} />
+
+                    <label htmlFor="email" className={styles.labelForm}>Email</label>
+                    <input id="email" type="email" className={styles.inputForm} />
+                    
+                    <label htmlFor="username" className={styles.labelForm}>Nome de usuário</label>
+                    <input id='username' type="text" className={styles.inputForm} />
+
+                    <label htmlFor="passwordOne" className={styles.labelForm}>Senha</label>
+                    <input id='passwordOne' type="password" className={styles.inputForm} />
+
+                    <label htmlFor="passwordTwo" className={styles.labelForm}>Confirmar senha</label>
+                    <input id='passwordTwo' type="password" className={styles.inputForm} />
+                </form>
+            </main>
+        </>
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './Login/Login'
+// import Login from './Login/Login'
 import './index.css'
+import Register from './Register/Register';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Register />
   </React.StrictMode>
 );
