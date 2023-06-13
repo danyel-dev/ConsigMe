dbac49b178191a042a57ef32c95fa4ed1ba67834
danyel-dev <dannykill4221x@gmail.com>
2023-06-12 23:08:22 -0300

    building the header of home page

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -13,24 +13,43 @@ export default function Home() {
         setModal(false)
     }
 
+    function handleChangeMenu(e) {
+        const menu = document.getElementById('menu')
+        
+        if(!(menu.style.display === "none"))
+            menu.style.display = "none"
+        else 
+            menu.style.display = "flex"
+    }
+
     return (
         <>
-            <nav className={styles.navbar}>
-                <h1>ConsigMe</h1>
+            <header className={styles.header}>
+                <div className={styles.logoMenu}>
+                    <h1>ConsigMe</h1>
+                    
+                    <button className={styles.buttonHamburguer} onClick={handleChangeMenu}>
+                        <span className="material-symbols-outlined">
+                            menu
+                        </span>
+                    </button>
+                </div>
 
-                <ul className={styles.ul}>
-                    <li className={styles.li}>Home</li>
-                    <li className={styles.li}>Sobre</li>
-                    <li className={`${styles.li} ${styles.liDropdown}`} onMouseEnter={modalVisible} onMouseOut={modalNotVisible}>
-                        Feito para você
-                        <span className="material-symbols-outlined">arrow_drop_down</span>
+                <nav id='menu' className={styles.navbar}>
+                    <ul className={styles.ul}>
+                        <li className={styles.li}>Home</li>
+                        <li className={styles.li}>Sobre</li>
+                        <li className={`${styles.li} ${styles.liDropdown}`} onMouseEnter={modalVisible} onMouseOut={modalNotVisible}>
+                            Feito para você
+                            <span className="material-symbols-outlined">arrow_drop_down</span>
 
-                        {modal? <div className={styles.modalHeaderLink}></div>: null}
-                    </li>
-                    <li className={styles.li}>Funcionalidades</li>
-                    <li className={styles.li}>Sair</li>
-                </ul>
-            </nav>
+                            {modal? <div className={styles.modalHeaderLink}></div>: null}
+                        </li>
+                        <li className={styles.li}>Funcionalidades</li>
+                        <li className={styles.li}>Sair</li>
+                    </ul>
+                </nav>
+            </header>
         </>
     );
 }
