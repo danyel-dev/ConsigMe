c90b271e835f310de2068cace83919c1655177b9
danyel-dev <dannykill4221x@gmail.com>
2023-06-12 21:13:08 -0300

    a

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import styles from './home.module.css'
 
 
 export default function Home() {
+    const [modal, setModal] = useState(false);
+
+    function modalVisible() {
+        setModal(true)
+    }
+
+    function modalNotVisible() {
+        setModal(false)
+    }
+
     return (
         <>
             <nav className={styles.navbar}>
@@ -10,9 +21,11 @@ export default function Home() {
                 <ul className={styles.ul}>
                     <li className={styles.li}>Home</li>
                     <li className={styles.li}>Sobre</li>
-                    <li className={`${styles.li} ${styles.liDropdown}`}>
+                    <li className={`${styles.li} ${styles.liDropdown}`} onMouseEnter={modalVisible} onMouseOut={modalNotVisible}>
                         Feito para você
-                        <span class="material-symbols-outlined">arrow_drop_down</span>
+                        <span className="material-symbols-outlined">arrow_drop_down</span>
+
+                        {modal? <div className={styles.modalHeaderLink}></div>: null}
                     </li>
                     <li className={styles.li}>Funcionalidades</li>
                     <li className={styles.li}>Sair</li>
