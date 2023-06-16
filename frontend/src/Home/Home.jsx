2a83390a59dc5ecc381821e185c5f727d47dae77
danyel-dev <dannykill4221x@gmail.com>
2023-06-16 15:40:39 -0300

    adding background image to home page

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -1,8 +1,17 @@
 import Header from '../Components/Header'
+import styles from './home.module.css'
 
 
 export default function Home() {
     return(
-        <Header />
+        <>
+            <section className={styles.section}>
+                <img src="" alt="" />
+                <Header />
+                <h1>ConsigMe</h1>
+                <p className={styles.section_p}>Seu sistema de gerenciamento de venda de produtos por consignação, feito para revendedores consignados e para consumidores.</p>
+                <button className={styles.section_button}>Saiba mais</button>
+            </section>
+        </>
     );
 }
