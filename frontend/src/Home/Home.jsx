fd495a749219e1550b97c38aa24f149661deb6ba
danyel-dev <dannykill4221x@gmail.com>
2023-06-16 17:04:59 -0300

    banner added

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -6,11 +6,13 @@ export default function Home() {
     return(
         <>
             <section className={styles.section}>
-                <img src="" alt="" />
                 <Header />
-                <h1>ConsigMe</h1>
-                <p className={styles.section_p}>Seu sistema de gerenciamento de venda de produtos por consignação, feito para revendedores consignados e para consumidores.</p>
-                <button className={styles.section_button}>Saiba mais</button>
+
+                <div className={styles.section_div}>
+                    <h1 className={styles.section_title}>ConsigMe</h1>
+                    <p className={styles.section_p}>Seu sistema de gerenciamento de venda de produtos por consignação, feito para revendedores consignados e para consumidores.</p>
+                    <button className={styles.section_button}>Saiba mais</button>
+                </div>
             </section>
         </>
     );
